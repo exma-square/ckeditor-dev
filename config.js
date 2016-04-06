92bb6da06aef46b47b18db478fc2dd09642a6fa2
Ai-Lin Liou <alin.code@gmail.com>
2016-04-06 15:28:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,63 +9,63 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
 	// %REMOVE_START%
 	config.plugins =
-		'about,' +
-		'a11yhelp,' +
-		'basicstyles,' +
-		'bidi,' +
-		'blockquote,' +
-		'clipboard,' +
-		'colorbutton,' +
-		'colordialog,' +
-		'contextmenu,' +
-		'dialogadvtab,' +
-		'div,' +
-		'elementspath,' +
-		'enterkey,' +
-		'entities,' +
-		'filebrowser,' +
-		'find,' +
-		'flash,' +
-		'floatingspace,' +
-		'font,' +
-		'format,' +
-		'forms,' +
-		'horizontalrule,' +
-		'htmlwriter,' +
-		'image,' +
-		'iframe,' +
-		'indentlist,' +
-		'indentblock,' +
-		'justify,' +
-		'language,' +
-		'link,' +
-		'list,' +
-		'liststyle,' +
-		'magicline,' +
-		'maximize,' +
-		'newpage,' +
-		'pagebreak,' +
-		'pastefromword,' +
-		'pastetext,' +
-		'preview,' +
-		'print,' +
-		'removeformat,' +
-		'resize,' +
-		'save,' +
-		'selectall,' +
-		'showblocks,' +
-		'showborders,' +
-		'smiley,' +
-		'sourcearea,' +
-		'specialchar,' +
-		'stylescombo,' +
-		'tab,' +
-		'table,' +
-		'tabletools,' +
-		'templates,' +
-		'toolbar,' +
-		'undo,' +
-		'wysiwygarea';
+			// 'about,' +
+			// 'a11yhelp,' +
+			// 'basicstyles,' +
+			// 'bidi,' +
+			// 'blockquote,' +
+			// 'clipboard,' +
+			// 'colorbutton,' +
+			// 'colordialog,' +
+			// 'contextmenu,' +
+			// 'dialogadvtab,' +
+			// 'div,' +
+			// 'elementspath,' +
+			// 'enterkey,' +
+			// 'entities,' +
+			'filebrowser,' +
+			// 'find,' +
+			// 'flash,' +
+			'floatingspace,' +
+			'font,' +
+			'format,' +
+			// 'forms,' +
+			// 'horizontalrule,' +
+			'htmlwriter,' +
+			'image,' +
+			// 'iframe,' +
+			'indentlist,' +
+			'indentblock,' +
+			'justify,' +
+			// 'language,' +
+			'link,' +
+			'list,' +
+			'liststyle,' +
+			'magicline,' +
+			'maximize,' +
+			// 'newpage,' +
+			// 'pagebreak,' +
+			// 'pastefromword,' +
+			// 'pastetext,' +
+			// 'preview,' +
+			// 'print,' +
+			// 'removeformat,' +
+			'resize,' +
+			// 'save,' +
+			// 'selectall,' +
+			// 'showblocks,' +
+			'showborders,' +
+			// 'smiley,' +
+			// 'sourcearea,' +
+			// 'specialchar,' +
+			'stylescombo,' +
+			'tab,' +
+			// 'table,' +
+			// 'tabletools,' +
+			// 'templates,' +
+			'toolbar,' +
+			// 'undo,' +
+			'wysiwygarea';
 	// %REMOVE_END%
 
 	// set the route of image upload route here
